4b44db6fab9bbfa9597027e72ecf2bad200f7716
CrankyTitanO7 <nedajtheawesome@gmail.com>
2026-06-21 00:58:01 -0400

    whiteboard draft v1.1 corrections

--- components/elements/topbar.js
+++ components/elements/topbar.js
@@ -14,8 +14,7 @@ class Topbar {
       { label: '🔧 Setup', page: 'setup.html', key: 'setup' },
       { label: '⚙️ Settings', page: 'settings.html', key: 'settings' },
       { label: 'mission control', page: 'mission_control.html', key: 'mission_control' },
-      { label: 'multimodel designer', page: 'multicon.html', key: 'multicon' },
-      { label: '✏️ whiteboard', page: 'whiteboard.html', key: 'whiteboard' },
+      { label: '✏️ multimodel designer', page: 'multicon.html', key: 'multicon' },
       { label: 'database explorer', page: 'db_explorer.html', key: 'db_explorer' },
       { label: 'export model', page: 'export.html', key: 'export' }
     ];
